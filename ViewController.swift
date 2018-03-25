7980a75d2a70d3971ca09f877cad68ebe0c19e80
larso077 <35981027+larso077@users.noreply.github.com>
2018-03-25 15:13:53 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW5-stackview-larson
+//
+//  Created by Derrek Larson on 3/25/18.
+//  Copyright © 2018 Derrek Larson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
